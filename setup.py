05548f08bd16c3ec72bac01751764fa942df4270
Neil Goldman <neilgoldman@outlook.com>
2020-09-25 13:05:17 -0400

    update setup.py with correct url and naming

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="advanced_logger-neilgoldman305",  # Replace with your own username
-    version="0.1.0",
+    name="python-advanced-logger",  # Replace with your own username
+    version="0.1.1",
     author="Neil Goldman",
     author_email="neil@everymundo.com",
     description="A logger module with a few extra quality-of-life features not available in the base logger module",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/everymundo/python_advanced_logger",
+    url="https://github.com/everymundo/python-advanced-logger",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
