609e03bef0f955562135abfdce5c83684c5c0eb5
Neil Goldman <neilgoldman@outlook.com>
2020-11-17 06:53:56 -0500

    bugfix previous commit: should be AdvancedJSONEncoder, not advanced_json_encoder

--- advanced_logger/__init__.py
+++ advanced_logger/__init__.py
@@ -1,4 +1,4 @@
 from .advanced_logger import register_logger, deregister_logger, clear_all_loggers, \
     initialize_logger_settings, basic_config, set_global_log_level, \
     AdvancedLogger, random_chance
-from .json_encoder import advanced_json_encoder
+from .json_encoder.advanced_json_encoder import AdvancedJSONEncoder
